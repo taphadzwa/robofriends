a20f9873560f05e2f582669ac9aa3d0da11d14ec
taphadzwa <tafadzwafm@gmail.com>
2020-09-23 14:05:47 +0200

    update card component

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Card from './Card';
+import Card from './components/Card';
 import App from './containers/App';
 import * as serviceWorker from './serviceWorker';
 import 'tachyons';
